d65bbd2a9f823ea94d63510741c10d85fce97af3
Szczepan <szczep.kowalik@gmail.com>
2022-02-19 19:42:57 +0100

    basic structure

---
+++ app/controllers/FormController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\controllers;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+class FormController
+{
+
+}
---
+++ app/resources/templates/template_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+<style>
+    h1 {
+        color: red;
+    }
+</style>
+</head>
+<body>
+<h1> A resume template </h1>
+<ul>
+    <li> skill 1 </li>
+    <li> skill 2 </li>
+    <li> skill 3 </li>
+    <li> skill 4 </li>
+    <li> skill 5 </li>
+</ul>
+
+</body>
+</html>
